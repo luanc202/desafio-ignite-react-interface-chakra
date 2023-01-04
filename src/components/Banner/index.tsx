b5fa82feb943b498b943d9b2a82bd90d9826d5b9
Luan <luan@luan-desktop>
2023-01-04 19:44:32 -0300

    Banner finished

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,28 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
 
 export function Banner() {
   return (
-    <Box w='100%' h='21rem' bgImg='/Background.png' bgRepeat='no-repeat' bgPosition='center' >
-
-    </Box>
+    <Flex
+      justifyContent='center'
+      w='100%'
+      h='21rem'
+      bgImg='/Background.png'
+      bgRepeat='no-repeat'
+      bgPosition='center'
+      gap='13rem'
+    >
+      <Flex
+        flexDir='column'
+        textColor='#F5F8FA'
+        justify='center'
+        gap='1.25rem'
+      >
+        <Text fontWeight='500' fontSize='4xl' noOfLines={2}>5 Continentes,<br />infinitas possibilidades.</Text>
+        <Text>Chegou a hora de tirar do papel a viagem que você<br />sempre sonhou.</Text>
+      </Flex>
+      <Box>
+        <Image transform='rotate(3deg)' display='inline' mb='-22rem' src='/Airplane.svg' alt='plane logo' />
+      </Box>
+    </Flex>
   )
 }
